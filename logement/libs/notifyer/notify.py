99295cd0dbdf5610782d34dbe58bdf9a625b28b1
fanch <francois_gautrais@msn.com>
2022-04-09 14:23:26 +0200

    Poll script

--- logement/libs/notifyer/notify.py
+++ logement/libs/notifyer/notify.py
@@ -1,6 +1,10 @@
 from django.http import HttpRequest
+from django.conf import settings
 
+def mail_notify(nb):
+    data = { k:v  for k,v in settings.SMTP.items()}
+    data["headers"]={
+        "Subject" : f"{nb} nouveaux "
+    }
 
 def notify(req : HttpRequest, annocnes):
-
-    pass
