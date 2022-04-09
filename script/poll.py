8cea374b6dd602153270f282d3f80e282af234a1
fanch <francois_gautrais@msn.com>
2022-04-09 13:28:43 +0200

    Poll script

--- script/poll.py
+++ script/poll.py
@@ -1,6 +1,10 @@
 #!/bin/env python3
 
 import sys
+from datetime import datetime
+from pathlib import Path
+
 import requests
 x = requests.get(sys.argv[1])
 print(x.content.decode())
+(Path(__file__).parent.parent / "poll.done").write_text(str(datetime.now()))
